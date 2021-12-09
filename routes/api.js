0f6c2a854f4a6e2c276ad95a0c4465fff7daa9ab
jandyme18 <69596572+jandyme18@users.noreply.github.com>
2021-12-09 21:06:13 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ router.use('/auth', require('../middleware/api_auth'));
 router.use('/report', require('../middleware/api_report'));
 router.use('/product', require('../middleware/api_product'));
 router.use('/discount', require('../middleware/api_discount'));
-router.use('/user', require('../middleware/api_discount'));
+router.use('/user', require('../middleware/api_user'));
 
 //จริง ๆ แล้ว discount ต้องอยู่ในหน้า dashboard แต่ยังไม่ได้เริ่มก็เลยทำแบบนี้ไปก่อน
 module.exports = router;
