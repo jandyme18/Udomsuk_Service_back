7c68251520c349d4a8230b07926a5dd746016356
jandyme18 <69596572+jandyme18@users.noreply.github.com>
2021-12-09 21:07:56 +0700

    Update api_user.js

--- middleware/api_user.js
+++ middleware/api_user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-const User = require('../models/user_schema')
+const Users = require('../models/user_schema')
 const crypto = require("crypto");
 
 router.get('/', async (req, res) => {
